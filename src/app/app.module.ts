6b347af7ecc49faff4f395bf26b6d81dd784e4b3
mondanglimbong <rumondangaslimbong@gmail.com>
2020-10-09 19:34:04 +0700

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -9,10 +10,16 @@ import { PeopleComponent } from './people/people.component';
 import { DocumentComponent } from './document/document.component';
 import { ActivityComponent } from './activity/activity.component';
 import { ChannelComponent } from './channel/channel.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, VideosComponent, PeopleComponent, DocumentComponent, ActivityComponent, ChannelComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: DocumentComponent },
+    ]) ],
+  declarations: [ AppComponent, HelloComponent, VideosComponent, PeopleComponent, DocumentComponent, ActivityComponent, ChannelComponent, TopBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
